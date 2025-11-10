f8e42c9ebbb606a0b6695e42cb3c5c930eb8b31d
Alexey Rybakov <r262363@yandex.ru>
2025-11-10 19:31:14 +0300

    Added ModuleId type and its support in Manifest

--- arcella-types/src/error.rs
+++ arcella-types/src/error.rs
@@ -7,6 +7,7 @@
 // This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use std::fmt;
 use thiserror::Error;
 
 /// Result type alias for `arcella-wasmtime` operations.
@@ -20,3 +21,22 @@ pub enum ArcellaTypeError {
     Manifest(String),
 
 }
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ArcellaModuleIdError {
+    InvalidFormat(String),
+    InvalidName(String),
+    InvalidVersion(String),
+}
+
+impl fmt::Display for ArcellaModuleIdError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ArcellaModuleIdError::InvalidFormat(s) => write!(f, "Invalid module ID format: '{}'. Expected 'name@1.2.3'", s),
+            ArcellaModuleIdError::InvalidName(s) => write!(f, "Invalid module name: '{}'. Must be alphanumeric with '-' or '_'", s),
+            ArcellaModuleIdError::InvalidVersion(s) => write!(f, "Invalid version: '{}'. Must be 'x.y.z' with no leading zeros", s),
+        }
+    }
+}
+
+impl std::error::Error for ArcellaModuleIdError {}
